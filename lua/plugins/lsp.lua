d960d5f5c18449a15aa07d596fe0e9df4aa9cbb7
steev-tupanwael <steev_tupanwael@outlook.com>
2025-01-04 15:51:52 +0900

    last update add dim from snack

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,10 +26,12 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    dependencies = { 'saghen/blink.cmp' },
 
+    config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
       local lspconfig = require("lspconfig")
+      
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
         settings = {
